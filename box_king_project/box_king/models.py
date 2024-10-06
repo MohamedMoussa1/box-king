5e8a7c9f2aa8fe567df492195d25a65b815296bd
Mohamed Moussa <mohamed.moussa@mail.utoronto.ca>
2024-10-06 16:55:52 -0400

    feat: Create Box, QRCode, Item models

--- box_king_project/box_king/models.py
+++ box_king_project/box_king/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Box(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    box_name = models.CharField(max_length=30, blank=False)
+    box_description = models.CharField(max_length=200)
+
+class QR_Code(models.Model):
+    box = models.OneToOneField(Box, on_delete=models.CASCADE, related_name='qr_code')
+    pdf_file = models.FileField(upload_to='qr_codes')
+
+class Item(models.Model):
+    box = models.ForeignKey(Box, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=50)
+    quantity = models.PositiveIntegerField(default=1)
--- box_king_project/box_king_project/settings.py
+++ box_king_project/box_king_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'box_king.apps.BoxKingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
