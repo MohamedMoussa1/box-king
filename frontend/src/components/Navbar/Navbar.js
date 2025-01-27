401998c8d6840b38c12fb3e5e78d033bf75637c8
Mohamed Moussa <mohamed.moussa@mail.utoronto.ca>
2025-01-27 17:31:39 -0500

    feat([DEV-20]): Add login and register buttons to navbar

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,13 +1,25 @@
 import "./Navbar.css";
-import { Box } from "@mui/material";
+import { Box, Button } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <Box className="navbar-container">
-      <NavLink to="/" className="banner-button">
+      <NavLink to="/" className="banner">
         Box King
       </NavLink>
+      <Box className="navbar-right">
+        <NavLink to="/login" className="navbar-link">
+          <Button variant="contained" className="navbar-button login-button">
+            Login
+          </Button>
+        </NavLink>
+        <NavLink to="/register" className="navbar-link">
+          <Button variant="contained" className="navbar-button register-button">
+            Get Started
+          </Button>
+        </NavLink>
+      </Box>
     </Box>
   );
 };
